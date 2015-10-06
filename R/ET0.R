9d74b72e73966988fdd436ff169ce3c5cfad5914
nFrechen <nanu.frechen@b-tu.de>
2015-10-06 13:07:55 +0200

    calculate net radiation either with given global radiation or with global radiation derived from sunshine duration documentation is still missing

--- R/ET0.R
+++ R/ET0.R
@@ -1,29 +1,50 @@
 # Berechnung der Grasreferenzverdunstung
-net.radiation <- function (sunshine, temp, U, date, latitude, albedo) {
-	phi <- latitude
-	alpha <- albedo
-	
-	xi <-  0.0172 * yday(date) - 1.39        
-	
-	R_0 <- 2425 + 1735 * sin(xi) + 44 * (phi - 51.0) * (sin(xi)-1)  
-	
-	S_0 <- 12.3 + sin(xi) * (4.3 + (phi - 51.0)/6)
-	
-	S_r <- sunshine/S_0 
-	
-	R_G <- R_0 * (0.19 + 0.55 * S_r)
-	
-	R_L <- 10.8 + 0.205 * temp 
-	
-	RnL <- R_L * (1.64*R_G/R_0-0.22) * (0.34 - 0.0044 * sqrt( U * es(temp) ))
-	
-	L <- 249.8 - 0.242 * temp
-	
-	RnK <- (1-alpha)  * R_G/L
-	
-	Rn <- RnK - RnL
-	return(Rn)
+Rn <- function(R_G=NA, sunshine=NA, doy=NA, date=NA, temp, U, latitude, albedo=0.23){
+	if(is.na(doy) & is.na(date)) stop("Either doy or date have to be given")
+	if(is.na(doy)) doy = yday(date)
+	Rn_K(R_G, temp, sunshine, doy, latitude, albedo) - Rn_L(R_G, latitude, doy, temp, U, sunshine)
 }
+
+Rn_K <- function(R_G=NA, temp, sunshine=NA, doy, latitude, albedo=0.23){
+	if(is.na(R_G) & is.na(sunshine)) stop("Either global radiation (R_G) or sunshine duration (sunshine) have to be given")
+	if(!is.na(R_G) & !is.na(sunshine)) warning("sunshine duration (sunshine) is neglected since global radiation (R_G) is given")
+	if(!is.na(R_G)) {
+	}else{
+		warning("Calculating global radiation (R_G) from sunshine duration")
+		R_G = R_G(sunshine, doy, latitude)
+		sunshine = NA
+	}
+	L = 249.8 - 0.242 * temp # [J/(cm^2*mm)]
+	Rn_K = (1-albedo)*R_G/L # [mm/d]
+}
+
+xi <- function(doy){
+	xi = 0.0172 * doy - 1.39 # [radiant] doy = day of year
+}
+
+R_0 <- function(doy, latitude){
+	R_0 = 2425 + 1735 * sin(xi(doy)) + 44 * (latitude - 51.0) * (sin(xi(doy)) -1)
+}
+
+Rn_L <- function(R_G=NA, latitude, doy, temp, U, sunshine){
+	if(is.na(R_G)) R_G = R_G(sunshine, doy, latitude)
+	# phi = geographic latitude [°]
+	R_L = 10.8 + 0.205 * temp # approximation
+	Rn_L = R_L * (1.64*R_G/R_0(doy, latitude) - 0.22) * (0.34 - 0.0044 * sqrt(U*es(temp))) # [mm/d]
+}
+
+R_G <- function(sunshine, doy, latitude){
+	S = sunshine # sunshine duration [h/d]
+	# S_0 = astronomical possible sunshine duration [h/d]
+	S_r = S/S_0(doy, latitude) # [-]
+	R_G = R_0(doy, latitude) *(0.19 + 0.55 * S_r) # [J/(cm^2*d)]
+}
+
+S_0 <- function(doy, latitude){
+	S_0 = 12.3 + sin(xi(doy)) * (4.3 + (latitude - 51.0)/6) # [h/d]
+}
+
+
 
 gammastar <- function(v_2, gamma){
 	# calculates the modified psychrometer constant
@@ -49,11 +70,11 @@ s <- function(temp, ice=FALSE){
 
 
 
-calcET0 <- function(temp, date, sunshine, U, v, v_height=2, latitude, albedo=0.23, gamma=0.65){
+calcET0 <- function(temp, date=NA, doy=NA, R_G=NA, sunshine=NA, U, v, v_height=2, latitude, albedo=0.23, gamma=0.65){
 	v_2 <- v * 4.2 /(log(v_height) + 3.5)
 	
 	s(temp)/(s(temp) + gammastar(v_2, gamma)) * 
-		net.radiation(sunshine, temp, U, date, latitude, albedo) +
+		Rn(R_G, sunshine, doy, date, temp, U, latitude, albedo) +
 		90*gamma / (s(temp) + gammastar(v_2, gamma)) * 
 		es(temp) / (temp + 273) *
 		(1-U/100) * v_2
