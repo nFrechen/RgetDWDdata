5d47696710ebb7129bf521e9badeb759b7c19f00
nFrechen <nanu.frechen@b-tu.de>
2016-03-22 13:42:26 +0100

    added try additionally to the exit status of download.file() The shiny apps server threw an errow and stopped the program when not able to download. This should now be prevented by try().

--- R/getDWDdata.R
+++ R/getDWDdata.R
@@ -2,7 +2,14 @@ getDWDstations <- function(){
 	#library("XML") # not needed in package
 	DWDstationenURL <- "ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/daily/kl/recent/KL_Tageswerte_Beschreibung_Stationen.txt"
 	stationFile <- tempfile()
-	download.file(DWDstationenURL, stationFile)
+	
+	try(dexit <- download.file(DWDstationenURL, stationFile))
+	# Exit-Status des downloads überprüfen
+	if(dexit!=0){
+		message("Sorry, the station list could not be downloaded from ", DWDstationenURL)
+		return()
+	} 
+	
 	header <- read.table(stationFile, header = FALSE, sep = " ", stringsAsFactors = FALSE, nrows = 1)
 	Stationen <- head(suppressWarnings(read.fwf(stationFile, widths = diff(c(0,12,21,30,45,57,67,108,201)), strip.white=TRUE, stringsAsFactors = FALSE, header=FALSE, skip=2, fileEncoding = "ISO-8859-1", encoding= "ISO-8859-1", col.names = header)), -1) # use head(, -1) to cut away the last row which is incomplete
 	Stationen$Stations_id <- formatC(as.numeric(Stationen$Stations_id), width = 5, format = "d", flag = "0")
@@ -93,7 +100,7 @@ getDWDdata <- function(Messstelle, historisch=FALSE, Metadaten=FALSE){
 	zipfile <- tempfile("DWD_download_", fileext=".zip")
 	
 	# Zip-Datei runterladen
-	dexit <- download.file(downloadlink,zipfile)
+	try(dexit <- download.file(downloadlink,zipfile))
 	
 	# Exit-Status des downloads überprüfen
 	if(dexit!=0){
